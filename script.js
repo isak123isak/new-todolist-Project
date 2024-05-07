b25979f13306baad6e81bb2bc8eadfe015565a6b
isak123isak <isak123isak@hotmail.com>
2024-05-07 03:33:06 +0200

    TodoList

---
+++ script.js
@@ -0,0 +1,20 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+  
+    if (taskInput.value.trim() !== "") {
+      var li = document.createElement("li");
+      li.textContent = taskInput.value;
+      
+      var deleteButton = document.createElement("span");
+      deleteButton.textContent = " ❌";
+      deleteButton.classList.add("delete");
+      deleteButton.onclick = function() {
+        taskList.removeChild(li);
+      };
+  
+      li.appendChild(deleteButton);
+      taskList.appendChild(li);
+      taskInput.value = "";
+    }
+  }
